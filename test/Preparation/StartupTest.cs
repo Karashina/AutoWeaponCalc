949e2b5e09ecf5aa7d4fcbb5dea8086de50f7f43
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2023-05-14 00:10:23 +0900

    Update Preparation

---
+++ test/Preparation/StartupTest.cs
@@ -0,0 +1,44 @@
+﻿using CalcsheetGenerator;
+using CalcsheetGenerator.Common;
+
+namespace Test.Methods
+{
+    [Collection("コンソールテストケース")]
+    public class StartupTest
+    {
+
+        readonly Preparation _Preparation = Preparation.GetInstance();
+
+        public StartupTest()
+        {
+            Console.SetIn(new StringReader("a\n"));
+            _Preparation.SelectMode();
+        }
+
+        [Fact(DisplayName="正常パターン")]
+        public void Nomal()
+        {
+            _Preparation.Startup();
+            var data = String.Join(Environment.NewLine, new[]
+            {
+                "Jean",
+                "sword"
+            });
+            var input = new StringReader(data);
+            Console.SetIn(input);
+
+            UserInput expectOutput = new UserInput("Jean", "sword", "0", "y");
+            Assert.Equivalent(expectOutput, _Preparation.Startup());
+        }
+
+        [Fact(DisplayName="未入力が含まれるパターンの場合")]
+        public void ErrorInputContainsEmpty()
+        {
+            var input = new StringReader("\n\n");
+            Console.SetIn(input);
+            UserInput expectOutput = new UserInput("", "", "0", "y");
+            Assert.Equivalent(expectOutput,  _Preparation.Startup());
+        }
+    }
+}
+
